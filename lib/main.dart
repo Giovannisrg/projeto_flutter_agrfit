e591942eb2fc9667319f10236426223d26f3966a
Giovannisrg <giovanni.gemignani@outlook.com>
2026-03-29 23:48:22 -0300

    salvando

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,34 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  Widget build(BuildContext context){
+    return const MaterialApp(home: LoginPage());
   }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+Widget build(BuildContext context){
+  return MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(title: Text('Página de Login'), centerTitle: true,),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+            ],
+          ))
+      )
+    )
+  )
+}
 }

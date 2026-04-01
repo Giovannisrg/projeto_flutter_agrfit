8d31a93e109b45a0a9ace7682364d6233fd722a0
Giovannisrg <giovanni.gemignani@outlook.com>
2026-04-01 01:22:18 -0300

    Finalizando protótipo Tela Login

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(home: LoginPage());
   }
 }
@@ -19,22 +19,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _nomeUsuario = "";
+  String _senhaUsuario = "";
 
-Widget build(BuildContext context){
-  return MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(title: Text('Página de Login'), centerTitle: true,),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-            ],
-          ))
-      )
-    )
-  )
-}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFDAD3E8),
+        appBar: AppBar(
+          title: const Text(
+            'AGR Fit',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 250.0, vertical: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    height: 120,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                Center(
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                const Text('Usuário'),
+                const SizedBox(height: 5),
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Digite o nome de usuário:',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (valor) {
+                    setState(() {
+                      _nomeUsuario = valor;
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 20),
+
+                const Text('Senha'),
+                const SizedBox(height: 5),
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Digite sua senha:',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (valor) {
+                    setState(() {
+                      _senhaUsuario = valor;
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo[200],
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        String acessarLogin = "";
+                      });
+                    },
+                    child: const Text('Login'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
